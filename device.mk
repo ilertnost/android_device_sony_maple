18335b6f6912e2f82074b24e3dabe32dfc3df10d
Tuan Anh <tuan73176@gmail.com>
2026-04-26 06:59:19 +0000

    lilac: Import audio configurations
    Taken from 47.2.A.11.228.
    
    Change-Id: I1b5c98defac5c54e48fef3c2b6932f875d2b4fdc

--- device.mk
+++ device.mk
@@ -11,6 +11,11 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
