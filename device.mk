6acdba845cfdeccf5f79d89978115e929fb45fdd
Tuan Anh <tuan73176@gmail.com>
2026-04-29 06:53:40 +0000

    lilac: Set AAPT configs
    Change-Id: Ifb98a803dc93af63b3595b4de6b7fe7008e28854

--- device.mk
+++ device.mk
@@ -6,6 +6,11 @@
 # Include from common device tree
 $(call inherit-product, device/sony/yoshino-common/yoshino.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
