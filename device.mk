ed938e1d3ac3799294e9c070d8d412252a5d3cdc
Tuan Anh <tuan73176@gmail.com>
2026-04-29 07:07:25 +0000

    lilac: Build and import NFC configurations
    Taken from 47.2.A.11.228.
    
    Change-Id: I2df4a11fc88dada877681485ee3ade600905eaa7

--- device.mk
+++ device.mk
@@ -16,6 +16,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
